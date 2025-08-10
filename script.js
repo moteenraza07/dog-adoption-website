a0948a9477bf3eee1dd1d0b5499496e951963348
Moteen Raza <moteenraza@gmail.com>
2025-08-10 18:22:03 -0400

    Display name age gender and image. random dog generator button is working

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
 const client_id = "fM0jsW78aUTFEGAD5kpS5AHAKsKqyTb4N6mG3j0Pvc5jHQGCqZ";
 const client_secret = "A7mBvO70gpTESDyCI7gQmi5gSiNiBAY661beTRSH";
 
-document
-  .getElementsByClassName("dog-adopt")
-  .addEventListener("click", function () {
-    fetch("https://api.petfinder.com/v2/oauth2/token")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        displayData(data);
-      })
-      .catch((error) => console.error("Error Fetchin data", error));
-  });
+document.getElementById("find-me").addEventListener("click", function () {
+  fetch("https://api.petfinder.com/v2/oauth2/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}`,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const token = data.access_token;
+      return fetch(`https://api.petfinder.com/v2/animals?type=dog&limit=1`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Did not fetch dog info");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data.animals && data.animals.length > 0) {
+        const dog = data.animals[0];
+
+        const photoUrl =
+          dog.photos && dog.photos.length > 0
+            ? dog.photos[0].medium
+            : "https://via.placeholder.com/150";
+
+        document.getElementById("dog-display").innerHTML = `
+        <h3>Name: ${dog.name}</h3>
+        <p>Age: ${dog.age}</p>
+        <p>Gender: ${dog.gender}</p>
+        <img class="dog-photo" src="${photoUrl}" alt="${dog.name}">
+`;
+      }
+    })
+    .catch((error) => console.error("Error Fetching data", error));
+});
