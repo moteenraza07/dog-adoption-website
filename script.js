a487786ee5fec9c97c08ba63df59f42f0a5d961d
Moteen Raza <moteenraza@gmail.com>
2025-08-11 13:45:15 -0400

    added comments in script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// client id and secret passcode provided when creating account in pet finder api
 const client_id = "fM0jsW78aUTFEGAD5kpS5AHAKsKqyTb4N6mG3j0Pvc5jHQGCqZ";
 const client_secret = "A7mBvO70gpTESDyCI7gQmi5gSiNiBAY661beTRSH";
 
 document.addEventListener("DOMContentLoaded", function () {
   const find_me = document.getElementById("find-me");
+  // on click function that will fetch the api and display
+  // dog name, dog age, dog gender and dog image
   if (find_me) {
     find_me.addEventListener("click", function () {
       fetch("https://api.petfinder.com/v2/oauth2/token", {
@@ -12,6 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
         },
         body: `grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}`,
       })
+        // error handling
         .then((response) => {
           if (!response.ok) {
             throw new Error("Network response was not ok");
@@ -35,13 +39,14 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .then((data) => {
           if (data.animals && data.animals.length > 0) {
+            // create a variable that gets the first photo of the dog image
             const dog = data.animals[0];
 
             const photoUrl =
               dog.photos && dog.photos.length > 0
                 ? dog.photos[0].medium
                 : "https://via.placeholder.com/150";
-
+            // use inner html to display in dog-display container
             document.getElementById("dog-display").innerHTML = `
               <h3>Name: ${dog.name}</h3>
               <p>Age: ${dog.age}</p>
