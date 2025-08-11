7450a69ee8c9159eae2031e95a14acb58362b059
Moteen Raza <moteenraza@gmail.com>
2025-08-11 00:27:10 -0400

    created form.js file and created variables for the form submission

---
+++ form.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("form");
+  const userName = document.getElementById("fullname");
+  const email = document.getElementById("email");
+  const phone = document.getElementById("phone");
+  const pet_name = document.getElementById("pet-name");
+  const message = document.getElementById("message");
+  const date = document.getElementById("preferred-contact");
+  const image = document.getElementById("attachment");
+  const checkbox = document.getElementById("form-checkbox");
+});
